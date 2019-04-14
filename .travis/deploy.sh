f043ccbffb164b455a4e941b7d35f4d462134c20
jensdufour <jens.dufour@outlook.com>
2019-04-14 18:23:49 +0800

    Update Travis-CI Deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 git clone --depth=1 --branch=gh-pages "https://github.com/jensdufour/BAP" pages
 cp $TRAVIS_BUILD_DIR/bachproef/bachproef-tin.pdf pages/docs/BP.pdf
+cp $TRAVIS_BUILD_DIR/poster/conference_poster.pdf pages/docs/BP_POSTER.pdf
+cp $TRAVIS_BUILD_DIR/voorstel/dufour_jens_voorstel.pdf pages/docs/BP_PROPOSAL.pdf
+cp $TRAVIS_BUILD_DIR/presentatie/HOGENT-presentatie.pdf pages/docs/BP_PRESENTATION.pdf
 cd pages
 git remote rm origin
 git remote add origin https://jensdufour:$GH_TOKEN@github.com/jensdufour/BAP.git
