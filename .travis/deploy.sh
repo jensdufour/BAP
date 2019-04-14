400d69936ac7c1cd59dcadf02b2d8215839824d4
jensdufour <jens.dufour@outlook.com>
2019-04-14 17:38:07 +0800

    Update Travis-CI Deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,6 +2,8 @@
 git clone --depth=1 --branch=master "https://github.com/jensdufour/BAP" pages
 cp bachproef/bachproef-tin.pdf pages/docs/BP.pdf
 cd pages
+git remote rm origin
+git remote add origin https://jensdufour:$GH_TOKEN@github.com/jensdufour/BAP.git
 git add .
 git config user.name "Travis CI"
 git config user.email "travis@travis-ci.org"
